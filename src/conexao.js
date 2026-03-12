59a8ce8db23212fc9a7ae9cd986b4e2e92da6627
fabiorochaufsc <fabio.rocha@gmail.com>
2026-03-12 00:50:42 -0300

    limpeza no conexao.js

--- src/conexao.js
+++ src/conexao.js
@@ -5,10 +5,7 @@ mongoose.start = async function () {
   this.connect(process.env.PROJ_MONGO_SERVER + process.env.PROJ_DB, {})
     .then(() =>
       console.log(
-        "Conectado: MongoDB:" +
-          process.env.PROJ_MONGO_SERVER +
-          "  " +
-          process.env.PROJ_DB,
+        `Conectado: MongoDB: ${process.env.PROJ_MONGO_SERVER} ${process.env.PROJ_DB}`,
       ),
     )
     .catch((err) => log("Erro ao conectar ao MongoDB", err));
